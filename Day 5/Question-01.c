2aa7e10fad0f67c92068d990a95506980eecb3fa
Kashika-18 <kashika.37853@stu.upes.ac.in>
2026-08-14 19:07:17 +0530

    Create Question-01.c

---
+++ Day 5/Question-01.c
@@ -0,0 +1,32 @@
+// Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 5 2
+Output 1:
+Simple Interest=100, Compound Interest=102.5
+
+Input 2:
+5000 7 3
+Output 2:
+Simple Interest=1050, Compound Interest=1125.76
+
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    float p, r, t ;
+    /* where, p = principal,
+    r = rate of interest
+    t = time*/
+    printf("Enter p, r, t = ");
+    scanf("%f %f %f", &p, &r, &t);
+
+    printf("Simple Interest = %.0f\n", (p*r*t)/100);
+    printf("Compound Interest = %.2f\n", p * pow(1+r/100,t)-p);
+
+    return 0;
+}
