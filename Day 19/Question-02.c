8304f096aa69afec86e8d988238c05ffe1630ec0
Kashika-18 <kashika.37853@stu.upes.ac.in>
2026-08-29 16:50:06 +0530

    Add sum of digits program in Question-02.c
    Implemented a program to calculate the sum of digits of a number.

---
+++ Day 19/Question-02.c
@@ -0,0 +1,31 @@
+//Q38: Write a program to find the sum of digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+*/
+
+#include<stdio.h>
+int main () {
+    int n, sum, rem;
+    printf("Enter a number = ");
+    scanf("%d", &n);
+
+    sum = 0;
+    while(n>0) {
+        rem = n%10;
+        sum = sum + rem;
+        n = n/10;
+    }
+    printf("%d", sum);
+    return 0;
+}
